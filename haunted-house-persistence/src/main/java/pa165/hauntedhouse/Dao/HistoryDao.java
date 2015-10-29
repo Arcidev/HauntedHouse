3c91c14f6ab3b60abc46816b4f362ee80733fce1
Luka <lucie.smidova@gmail.com>
2015-10-29 20:45:49 +0100

    Added Javadoc for History

--- haunted-house-persistence/src/main/java/pa165/hauntedhouse/Dao/HistoryDao.java
+++ haunted-house-persistence/src/main/java/pa165/hauntedhouse/Dao/HistoryDao.java
@@ -10,13 +10,52 @@ import pa165.hauntedhouse.Entity.History;
 
 /**
  *
- * @author Luka
+ * @author Lucie Smidova
  */
 public interface HistoryDao {
+    
+     /**
+     * Creates new history record
+     * 
+     * @param h
+     */
     public void create(History h);
+    
+    /**
+     * Deletes history record
+     * 
+     * @param h
+     */
     public void delete(History h);
+    
+    /**
+     * Updates history record
+     * 
+     * @param h
+     */
+    public void update(History h);
+    
+     /**
+     * Returns a history record with specified id from database
+     * 
+     * @param id
+     * @return the ability with specified id
+     */
     public History findById(int id);
+    
+     /**
+     * Returns a history record with specified date from database
+     * 
+     * @param Date
+     * @return the history record with specified date
+     */
     public History findByDate(java.util.Date Date);
+    
+    /**
+     * Returns list of all history records that are stored in database
+     * 
+     * @return the list of all history records
+     */
     public List<History> findAll();
     
 }
