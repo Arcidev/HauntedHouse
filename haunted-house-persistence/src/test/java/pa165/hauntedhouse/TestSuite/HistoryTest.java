ec6c8b8c51485db34b075f1a651d8b0ceb54c983
Milan <Milan@Milan>
2015-10-30 12:29:00 +0100

    HistoryTest

--- haunted-house-persistence/src/test/java/pa165/hauntedhouse/TestSuite/HistoryTest.java
+++ haunted-house-persistence/src/test/java/pa165/hauntedhouse/TestSuite/HistoryTest.java
@@ -6,10 +6,20 @@
 
 package pa165.hauntedhouse.TestSuite;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pa165.hauntedhouse.Dao.HistoryDao;
+import pa165.hauntedhouse.Entity.History;
+import pa165.hauntedhouse.Entity.Spook;
 import pa165.hauntedhouse.PersistenceApplicationContext;
 
 /**
@@ -18,6 +28,82 @@ import pa165.hauntedhouse.PersistenceApplicationContext;
  */
 @ContextConfiguration(classes = PersistenceApplicationContext.class)
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
 public class HistoryTest extends AbstractTestNGSpringContextTests{
     
+    @Autowired
+    private HistoryDao historyDao;
+
+    @PersistenceContext
+    private EntityManager em;    
+
+    @Test
+    public void haveToFindAll(){
+        History history1 = new History();
+        History history2 = new History();
+        
+        history1.setInfo("Info1");
+        history2.setInfo("Info2");
+        
+        historyDao.create(history1);
+        historyDao.create(history2);
+        List<History> history = historyDao.findAll();
+        
+        Assert.assertEquals(history.size(), 2);
+        Assert.assertTrue(history.contains(history1));
+        Assert.assertTrue(history.contains(history2));
+    }
+    
+    @Test
+    public void haveToFindByID() {
+        History history1 = new History();
+        history1.setInfo("Info1");
+
+        historyDao.create(history1);
+
+        Assert.assertEquals(historyDao.findById(history1.getID()), history1);
+    }
+    
+    @Test
+    public void haveToDelete(){
+        History history1 = new History();
+        History history2 = new History();
+        history1.setInfo("Info1");
+        history2.setInfo("Info2");
+        
+        historyDao.create(history1);
+        historyDao.create(history2);
+        
+        historyDao.delete(history2);
+        
+        List<History> history = historyDao.findAll();
+        
+        Assert.assertEquals(history.size(), 1);
+        Assert.assertTrue(history.contains(history1));
+    }
+    
+    @Test
+    public void haveToUpdate(){        
+        History history1 = new History();
+        history1.setInfo("InfoInfo");
+        
+        historyDao.create(history1);
+        Assert.assertEquals(history1.getInfo(), "InfoInfo");
+        history1.setInfo("Info2");
+        historyDao.update(history1);
+        Assert.assertEquals(history1.getInfo(), "Info2");  
+    }
+    
+    @Test
+    public void setSomeData(){
+        History history1 = new History();
+        Spook spook = new Spook();
+        spook.setName("Spook1");
+        
+        history1.setSpook(spook);
+        history1.setInfo("InfoInfo");
+        
+        Assert.assertEquals(history1.getSpook().getName(), "Spook1");
+        Assert.assertEquals(history1.getInfo(), "InfoInfo");
+    }
 }
