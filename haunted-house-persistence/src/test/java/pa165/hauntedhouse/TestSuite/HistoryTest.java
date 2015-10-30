919afec103d01f0ec046b0a0359e35e277dd4714
Milan <Milan@Milan>
2015-10-30 13:18:55 +0100

    HistoryTest

--- haunted-house-persistence/src/test/java/pa165/hauntedhouse/TestSuite/HistoryTest.java
+++ haunted-house-persistence/src/test/java/pa165/hauntedhouse/TestSuite/HistoryTest.java
@@ -7,8 +7,6 @@
 package pa165.hauntedhouse.TestSuite;
 
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
