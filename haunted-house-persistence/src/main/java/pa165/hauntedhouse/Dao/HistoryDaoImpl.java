42adb230fb3898500e18a74613d7c567ea4357d2
Luka <lucie.smidova@gmail.com>
2015-11-25 22:09:34 +0100

    Added search by date range

--- haunted-house-persistence/src/main/java/pa165/hauntedhouse/Dao/HistoryDaoImpl.java
+++ haunted-house-persistence/src/main/java/pa165/hauntedhouse/Dao/HistoryDaoImpl.java
@@ -59,4 +59,14 @@ public class HistoryDaoImpl implements HistoryDao{
         }
 
     }
+    
+    @Override
+    public List<History> searchByRange(java.util.Date historyDate1, java.util.Date historyDate2) {
+        try {
+            return em.createQuery("select h from History h where h.historyDate between :date1 and date2", History.class)
+                    .setParameter("date1", historyDate1).setParameter("date2", historyDate2).getResultList();
+        } catch (NoResultException nrf) {
+            return null;
+        }
+    }
 }
