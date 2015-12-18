fbf53b66534d639a26b4a765eee3f9e39b0657d6
Arci <Arcidev@users.noreply.github.com>
2015-12-18 22:11:37 +0100

    Error: added error handlers

---
+++ haunted-house-spring-mvc/src/main/java/pa165/hauntedhouse/MVC/Controllers/ErrorHandlerController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pa165.hauntedhouse.MVC.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import pa165.hauntedhouse.Exception.HttpNotFound;
+
+/**
+ *
+ * @author Andrej Dobes
+ */
+@Controller
+@ControllerAdvice
+public class ErrorHandlerController {
+    
+    @Autowired
+    private MessageSource messageSource; //resource bundle provided by Spring
+    
+    @RequestMapping(value = { "denied" }, method = RequestMethod.GET)
+    public String accessDenied(Model model) {
+        model.addAttribute("errorTitle", messageSource.getMessage("error.deniedTitle", null, LocaleContextHolder.getLocale()));
+        model.addAttribute("errorMessage", messageSource.getMessage("error.deniedMessage", null, LocaleContextHolder.getLocale()));
+        return "error";
+    }
+    
+    @ExceptionHandler(HttpNotFound.class)
+    public ModelAndView handleHttpNotFound(HttpNotFound ex) {
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("errorTitle", messageSource.getMessage("error.notFoundTitle", null, LocaleContextHolder.getLocale()));
+        model.addObject("errorMessage", messageSource.getMessage("error.notFoundMessage", null, LocaleContextHolder.getLocale()));
+        return model;
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(Exception ex) {
+        ModelAndView model = new ModelAndView("error");
+        model.addObject("errorTitle", messageSource.getMessage("error.title", null, LocaleContextHolder.getLocale()));
+        model.addObject("errorMessage", messageSource.getMessage("error.message", null, LocaleContextHolder.getLocale()));
+        return model;
+    }
+}
---
+++ haunted-house-spring-mvc/src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="utf-8" trimDirectiveWhitespaces="true" session="false" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="ability" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<ability:header>
+<jsp:attribute name="body">
+
+    <div class="jumbotron">
+        <h2>${errorTitle}</h2>
+        <p>${errorMessage}</p>
+    </div>
+    
+</jsp:attribute>
+</ability:header>
